a896013365dc641d522b4e54963481730222571e
BineetNaidu <bineet.s.naidu@gmail.com>
2020-07-15 19:10:36 +0530

    Refactor postRegister & errorHandler

--- middleware/index.js
+++ middleware/index.js
@@ -1,7 +1,5 @@
 module.exports = {
-    errorHandler: function (fn) {
-        (req, res, next) => {
-            Promise.resolve(fn(req, res, next)).catch(next);
-        };
+    errorHandler: (fn) => (req, res, next) => {
+        Promise.resolve(fn(req, res, next)).catch(next);
     },
 };
