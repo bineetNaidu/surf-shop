40f602386b415eda6b91bf985415af33377585c1
bineetNaidu <bineet.s.naidu@gmail.com>
2020-07-19 16:28:37 +0530

    REVIEWS init setup

---
+++ controllers/reviews.js
@@ -0,0 +1,12 @@
+const Post = require("../models/post");
+const Review = require("../models/review");
+
+module.exports = {
+    // create REVIEW
+    async reviewCreate(req, res, next) {},
+
+    // updating the REVEIW
+    async reviewUpdate(req, res, next) {},
+    // destroy REVIEWS
+    async reviewDestroy(req, res, next) {},
+};
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,24 +1,9 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 
-/* GET review index posts/:id/reviews */
-router.get("/", (req, res, next) => {
-    res.send("GET review index posts/:id/reviews");
-});
-
 /* POST review create posts/:id/reviews */
 router.post("/", (req, res, next) => {
     res.send("POST reviews create posts/:id/review");
-});
-
-/* GET review show /review/:id */
-router.get("/:review_id", (req, res, next) => {
-    res.send("GET reviews show posts/:id/review/:review_id");
-});
-
-/* GET review edit posts/:id/review/:id/edit */
-router.get("/:id/edit", (req, res, next) => {
-    res.send("GET reviews edit posts/:id/review/:id/edit");
 });
 
 /* PUT review update posts/:id/review/:id */
