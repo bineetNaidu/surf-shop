d734c7fdba4741bc3050f2cb82d38fc468e2070a
bineetNaidu <bineet.s.naidu@gmail.com>
2020-07-24 15:38:51 +0530

    add client JS for profile form validation

---
+++ public/javascripts/profile.js
@@ -0,0 +1,40 @@
+let newPasswordValue;
+let confirmationValue;
+const form = document.querySelector("form");
+const newPassword = document.getElementById("new-password");
+const confirmation = document.getElementById("password-confirmation");
+const validationMessage = document.getElementById("validation-message");
+
+// functions declaration
+function validatePassword(message, add, remove) {
+    validationMessage.textContent = message;
+    validationMessage.classList.add(add);
+    validationMessage.classList.remove(remove);
+}
+
+// DOM
+confirmation.addEventListener("input", (e) => {
+    e.preventDefault();
+    newPasswordValue = newPassword.value;
+    confirmationValue = confirmation.value;
+    if (newPasswordValue !== confirmationValue) {
+        validatePassword("password must match!", "color-red", "color-green");
+    } else {
+        validatePassword("password match!", "color-green", "color-red");
+    }
+});
+
+form.addEventListener("submit", (e) => {
+    if (newPasswordValue !== confirmationValue) {
+        e.preventDefault();
+        const error = document.getElementById("error");
+        if (!error) {
+            const flashErrorH1 = document.createElement("h1");
+            flashErrorH1.classList.add("color-red");
+            flashErrorH1.setAttribute("id", "error");
+            flashErrorH1.textContent = "Password must match!";
+            const navbar = document.getElementById("navbar");
+            navbar.parentNode.insertBefore(flashErrorH1, navbar.nextSibling);
+        }
+    }
+});
